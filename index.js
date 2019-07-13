34ec9c0e0fdd6b3c915a5c649c9b9daefce5a619
Ashish Kumar10 <ashish.kumar10@synapse.com>
2019-07-13 13:53:06 +0530

    new file addition

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+<!DOCTYPE html>
 <html>
 <body>
-<h1>123</h1>
+
+<h2>My First JavaScript778666</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
 </body>
-</html>
+</html> 

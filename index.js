fe978d8107d98ed5908d910d03c4925c0024f385
vikas <ashish.kumar10@synapse.com>
2019-07-14 12:32:30 +0530

    new code

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>My First JavaScript778666</h2>
-
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
-
-<p id="demo"></p>
-
-</body>
-</html> 
+document.write("Helli world")
